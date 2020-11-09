4197e1f5c79b5620527933f8e51c811a1bab4248
Dewey C <deweycraddock@gmail.com>
2020-11-08 23:08:55 -0600

    responsive 1

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+$(document).ready(function() {
+    $(".navbar-toggler").click(function(){
+    $(".collapse").collapse('toggle');
+});
+    $(".navbar-toggler").click(function() {
+    $(".collapse").collapse('show');
+    });
+    $(".navbar-toggler").click(function() {
+    $(".collapse").collapse('hide');
+    });
+});
+
+
+// $('.collapse').collapse()
